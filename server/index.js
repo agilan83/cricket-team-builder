7c4733977fcd493ad37bfd3cfdaa26b3dded72f9
Agilan Pandian <agilanpandian@Agilans-Mac-mini.local>
2026-01-19 21:58:59 +0400

    Serve index.html at root route

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});	
+
 const db = new sqlite3.Database("./server/db.sqlite");
 
 // Create tables
